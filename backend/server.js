e6f850a8bd3d012959e23acedb62e67e51ea7924
Aastha Pandey <rpap1987@gmail.com>
2026-06-21 16:07:09 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://workshop-webpage.vercel.app/",
+      "https://workshop-webpage.vercel.app",
     ],
     methods: ["GET", "POST"],
   })
