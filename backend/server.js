74c3a6e0b4f141bc0d1475811cee808f9257c3bd
Aastha Pandey <rpap1987@gmail.com>
2026-06-21 15:57:26 +0530

    Fixed registration API issue

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,12 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://workshop-webpage-b7e77bze6-rpap1987-gmailcoms-projects.vercel.app",
+      "https://workshop-webpage.vercel.app/",
     ],
     methods: ["GET", "POST"],
   })
